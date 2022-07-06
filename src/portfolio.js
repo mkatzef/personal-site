39405a531ba7598e8f436d215e54f92782af9dde
Marc Katzef <marckatzef@gmail.com>
2022-07-06 13:44:56 +1000

    Update section headings, animation duration

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your
 const splashScreen = {
   enabled: true, // set false to disable splash screen
   animation: splashAnimation,
-  duration: 1000 // Set animation duration as per your animation
+  duration: 900 // Set animation duration as per your animation
 };
 
 // Summary And Greeting Section
