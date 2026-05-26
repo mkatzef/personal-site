ae6501d71c3bea669f848b5ccb6cb1354c241f0d
Marc Katzef <mkatzef@Marcs-MacBook-Air.local>
2026-05-26 21:36:25 +1000

    Updates

---
+++ precache-manifest.e6e996d9de7bb622933588980e381d0b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85383ddd38f143a64f408be791c81a94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "454163aa6dbc281d921b",
+    "url": "/static/css/3.933c2378.chunk.css"
+  },
+  {
+    "revision": "33f6b5a1f28a204101f7",
+    "url": "/static/css/4.cd61716c.chunk.css"
+  },
+  {
+    "revision": "06129a04ba963d5cf394",
+    "url": "/static/css/main.b1d56b28.chunk.css"
+  },
+  {
+    "revision": "4b3583f769cd6e3bb088",
+    "url": "/static/js/2.97f8f51c.chunk.js"
+  },
+  {
+    "revision": "e69a64278ba4d5d08f34b5e1de4285e6",
+    "url": "/static/js/2.97f8f51c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "454163aa6dbc281d921b",
+    "url": "/static/js/3.ef2527b9.chunk.js"
+  },
+  {
+    "revision": "33f6b5a1f28a204101f7",
+    "url": "/static/js/4.231f7fd3.chunk.js"
+  },
+  {
+    "revision": "06129a04ba963d5cf394",
+    "url": "/static/js/main.3dbb7ae0.chunk.js"
+  },
+  {
+    "revision": "451d7702dd361aaa4c1a",
+    "url": "/static/js/runtime-main.7c2785bb.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "85bb5154c9850a16d73b1eb14e942d8b",
+    "url": "/static/media/astro.85bb5154.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "b4268fd60e043f4593e83940d1222d37",
+    "url": "/static/media/isgLogo.b4268fd6.png"
+  },
+  {
+    "revision": "b84179693a139e79c817a9012a64d4d1",
+    "url": "/static/media/litho-src.b8417969.jpg"
+  },
+  {
+    "revision": "ccb36dad35bb086f55168c9f310c49fa",
+    "url": "/static/media/litho.ccb36dad.jpg"
+  },
+  {
+    "revision": "258ddeec01a469ac548907b381b6c34c",
+    "url": "/static/media/mspLogo.258ddeec.png"
+  },
+  {
+    "revision": "dbde5c318b4149d801fa628aa7a41a29",
+    "url": "/static/media/osf.dbde5c31.png"
+  },
+  {
+    "revision": "c38450d1f313889d72ee8d56828e5552",
+    "url": "/static/media/otLogo.c38450d1.png"
+  },
+  {
+    "revision": "b4632de29b6a5cc7af4e0eb7111c69bc",
+    "url": "/static/media/scholar.b4632de2.png"
+  },
+  {
+    "revision": "a8b9baa3917695a98e76c19598e1fc8a",
+    "url": "/static/media/strava.a8b9baa3.png"
+  },
+  {
+    "revision": "2cbb31b66bed3245c96107ed94308b3a",
+    "url": "/static/media/ucLogo.2cbb31b6.png"
+  },
+  {
+    "revision": "e22bfc105005869ffdb575e74c2e1ec6",
+    "url": "/static/media/unswLogo.e22bfc10.png"
+  },
+  {
+    "revision": "4b74aca26885d78bced4cbf142a6cd1e",
+    "url": "/static/media/uoaLogo.4b74aca2.png"
+  },
+  {
+    "revision": "2808393920bf79e69759282d03558e67",
+    "url": "/static/media/uomLogo.28083939.png"
+  },
+  {
+    "revision": "10c965df05f121b50214223e893c6d36",
+    "url": "/static/media/vcLogo.10c965df.png"
+  },
+  {
+    "revision": "63b0f9f892dac081d20ba11f9c803ea5",
+    "url": "/static/media/walkplot.63b0f9f8.png"
+  },
+  {
+    "revision": "0d96018c932d7f04bf7f41f519389d55",
+    "url": "/static/media/wifi-mapper-map.0d96018c.jpg"
+  },
+  {
+    "revision": "74b42f4303cad4bad04432a3c1fd077c",
+    "url": "/static/media/wifi-mapper-scanner.74b42f43.jpg"
+  },
+  {
+    "revision": "c6e39af2f66061bb7f2c7dbcbf3ca59f",
+    "url": "/static/media/world-vote.c6e39af2.png"
+  }
+]);
